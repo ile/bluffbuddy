4df1dee9723833bcec63bd919ffc7669bafa934f
Ilkka Huotari <ilkkah@gmail.com>
2024-03-15 07:33:07 +0200

    ..

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,9 +1,9 @@
 <template>
-  <p>BluffBuddy v0.2.7</p>
+  <p>BluffDaddy v0.2.7</p>
   <p>Copyright (C) 2022 Wataru Inariba, (C) 2024 Ilkka Huotari</p>
   <p>
-    <a target="_blank" class="link" href="https://github.com/ile/bluffbuddy">
-      https://github.com/ile/bluffbuddy
+    <a target="_blank" class="link" href="https://github.com/ile/bluffdaddy">
+      https://github.com/ile/bluffdaddy
     </a>
   </p>
 </template>
